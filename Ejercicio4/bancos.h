e4e8e16057633e7ba4a1a46b0523a66f3f71f084
Tomasocana <tocana@udesa.edu.ar>
2025-04-06 14:51:25 -0300

    Homework terminado

--- Ejercicio4/bancos.h
+++ Ejercicio4/bancos.h
@@ -8,35 +8,31 @@ void menu();
 
 class Banco{
     public:
-        Banco(string titularCuenta) : titularCuenta(titularCuenta) {}
-        void depositar(double cant);
-        virtual double retirar(double cant) = 0;
+        Banco(string titularCuenta) : titularCuenta(titularCuenta) {} // Constructor
+        void depositar(double cant); // Función para depositar
+        virtual double retirar(double cant) = 0; // Función para retirar
     private:
-        virtual void mostrarInfo() = 0;
-        double balance;
-        
+        virtual void mostrarInfo() = 0; // Función para mostrar información de la cuenta
     protected:
-        string titularCuenta;
+        double balance = 0; // El balance de la cuenta de banco
+        string titularCuenta; // El titular de la cuenta de banco
 };
 
 
 class CajaDeAhorro : public Banco {
     public:
-        CajaDeAhorro(string titularCuenta) : Banco(titularCuenta) {}
+        CajaDeAhorro(string titularCuenta) : Banco(titularCuenta) {} // Constructor
         double retirar(double cant) override;
         void mostrarInfo() override;
-    private:
-        double balance;
 };
 
-
+// En este caso hago que en el constructor se pase una referencia a la caja de ahorro asociada al mismo titular
+// para poder acceder a esa cuenta en caso de que no haya dinero suficiente en la cuenta corriente para retirar
 class CuentaCorriente : public Banco{
     public: 
-        CuentaCorriente(string titularCuenta, CajaDeAhorro* caja) : Banco(titularCuenta), cajaAhorro(caja), balance(0) {}
+        CuentaCorriente(string titularCuenta, CajaDeAhorro* caja) : Banco(titularCuenta), cajaAhorro(caja) {} 
         double retirar(double cant) override;
         void mostrarInfo() override;
-        friend class CajaDeAhorro;
     private:
         CajaDeAhorro* cajaAhorro;
-        double balance;
 };
--- Ejercicio4/src.cpp
+++ Ejercicio4/src.cpp
@@ -7,16 +7,17 @@ void Banco::depositar(double cant){balance += cant;}
 // Clase Caja de Ahorro
 
 double CajaDeAhorro::retirar(double cant){
-    if(balance < cant) {
+    if(balance < cant) { // Si se intenta sacar mas dinero del que hay en el balance, muestra un error y devuelve 0$
         cout << "Error: Dinero insuficiente en la cuenta" << endl;
         return 0;
     }
-    balance -= cant;
+    cout << "Dinero retirado con éxito." << endl;
+    balance -= cant; // Sino retira el dinero y disminuye el balance
     return cant;
 }
 
 void CajaDeAhorro::mostrarInfo(){
-    static int i = 0;
+    static int i = 0; // Static int para mantener la cuenta de cuantas veces se muestra la información para descontar 20$ cuando se muestra 2 veces o más
     if (i >= 2) balance -= (double)20;
     cout << "El titular de la caja de ahorro es: " << titularCuenta << ".";
     cout << " Y tiene " << balance << "$ en su cuenta." << endl;
@@ -26,14 +27,15 @@ void CajaDeAhorro::mostrarInfo(){
 // Clase Cuenta Corriente
 
 double CuentaCorriente::retirar(double cant){
-    if(balance < cant){
+    if(balance < cant){ // Si se intenta retirar mas dinero del que hay en el balance, intenta retirar esa suma de dinero en la cja de ahorro
         cout << "Dinero insuficiente en la cuenta corriente. Accediendo a la caja de ahorro..." << endl;
-        if(cajaAhorro){
+        if(cajaAhorro){ // Ver si existe una caja de ahorro asociada
             cajaAhorro->retirar(cant);
         }
         else cout << "No hay caja de ahorro asociada. Imposible retirar dinero." << endl;
         return 0;
     }
+    cout << "Dinero retirado con éxito." << endl;
     balance -= cant;
     return cant;
 }
@@ -47,19 +49,26 @@ void menu(){
     cout << "Creando una caja de ahorro:" << endl;
     CajaDeAhorro c((string)"Tomas");
     cout << "Depositando 400 pesos..." << endl;
+    c.depositar(400);
     c.mostrarInfo();
     cout << endl;
+
     cout << "Creando una cuenta corriente:" << endl;
     CuentaCorriente b((string)"Tomas", &c);
     b.mostrarInfo();
     cout << "Depositando 200 pesos..." << endl;
-    b.depositar((double)200);
+    b.depositar(200);
     b.mostrarInfo();
+    cout << endl;
+
     cout << "Intenando retirar 300 pesos de la cuenta corriente:" << endl;
-    b.retirar((double)300);
+    b.retirar(300);
     b.mostrarInfo();
     c.mostrarInfo();
-    cout << "Como se mostró 2 veces la caja de ahorro, se descuentan 20 pesos, y asi sucesivamente..." << endl;
+    cout << "Se puede ver que disminuyó el balance de la caja de ahorro." << endl; 
+    cout << endl;
+
+    cout << "Además como se mostró 2 veces la caja de ahorro, se descuentan 20 pesos, y asi sucesivamente..." << endl;
     c.mostrarInfo();
     c.mostrarInfo();
 }
